d8eab51a0d67141ce1fc8e55ba11148099365e9f
Tate <thandley1234@gmail.com>
2023-02-02 11:37:58 -0700

    commit tree f3e0276eee5c6c826f0c332fb9f1776038ec054f

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        # custom_response_hash = {
+        #     user: user,
+        #     items: user.items
+        # }
+        render json: user, include: :items
+    end
 end
